a637c39e110bc744413489ae1150e9ed8a891982
34coopatroopa <cjhoy@iastate.edu>
2025-07-31 20:46:30 -0500

    phoneness pt12

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -207,7 +207,7 @@ export default function App() {
         <div
           style={{
             position: 'absolute',
-            bottom: '20px', left: '50%',
+            bottom: '70px', left: '50%',
             transform: 'translateX(-50%)', zIndex: 10,
           }}
         >
